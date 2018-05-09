7759f638bb3595e23d54eecbfbc7df44315b138a
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-09 00:48:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,11 +3,10 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle });
+  return Object.assign({}, playlist, {['artistName']: songTitle });
   
 }
 
-updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
